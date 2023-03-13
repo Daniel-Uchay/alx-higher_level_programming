a01ee86e5e5aa525c0cc3d686ae91b1833aa7c1f
Daniel-Uchay <meetcaxtroh@gmail.com>
2023-03-13 23:29:54 +0100

    edited

--- 0x12-javascript-warm_up/103-object_fct.js
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -4,13 +4,14 @@ const myObject = {
   value: 12
 };
 console.log(myObject);
-myObject.incr = function () {
-  this.value++;
-};
-myObject.incr();
-console.log(myObject);
-myObject.incr();
-console.log(myObject);
-myObject.incr();
-console.log(myObject);
 
+myObject.incr = function () {
+  myObject.value += 1;
+};
+
+myObject.incr();
+console.log(myObject);
+myObject.incr();
+console.log(myObject);
+myObject.incr();
+console.log(myObject);
